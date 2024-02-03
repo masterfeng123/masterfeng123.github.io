0d09cee9142a94d0d8ffcc202e2b9d06101d0b22
masterfeng123 <94738534+masterfeng123@users.noreply.github.com>
2024-02-04 00:44:31 +0800

    Add files via upload

---
+++ C語言.js
@@ -0,0 +1,334 @@
+function clicked1(){
+    const U00 = document.getElementById('U00');
+    const U01 = document.getElementById('U00-clicked');
+    const Q00 = document.getElementById('Q001');
+    const Q01 = document.getElementById('Q001-clicked');
+    if(U00){
+        U00.id='U00-clicked';
+        Q00.id='Q001-clicked';
+    }
+    else{
+        U01.id='U00';
+        Q01.id='Q001';
+    }
+}
+
+function clicked2(){
+    const U10 = document.getElementById('U01');
+    const U11 = document.getElementById('U01-clicked');
+    const Q101 = document.getElementById('Q101');
+    const Q101P = document.getElementById('Q101-clicked');
+    const Q102 = document.getElementById('Q102');
+    const Q102P = document.getElementById('Q102-clicked');
+    const Q103 = document.getElementById('Q103');
+    const Q103P = document.getElementById('Q103-clicked');
+
+    if(U10){
+        U10.id='U01-clicked';
+        Q101.id='Q101-clicked';
+        Q102.id='Q102-clicked';
+        Q103.id='Q103-clicked';
+    }
+    else{
+        U11.id='U01';
+        Q101P.id='Q101';
+        Q102P.id='Q102';
+        Q103P.id='Q103';
+    }
+}
+
+function clicked3(){
+    const U10 = document.getElementById('U02');
+    const U11 = document.getElementById('U02-clicked');
+    const Q101 = document.getElementById('Q201');
+    const Q101P = document.getElementById('Q201-clicked');
+    const Q102 = document.getElementById('Q202');
+    const Q102P = document.getElementById('Q202-clicked');
+    const Q103 = document.getElementById('Q203');
+    const Q103P = document.getElementById('Q203-clicked');
+
+    if(U10){
+        U10.id='U02-clicked';
+        Q101.id='Q201-clicked';
+        Q102.id='Q202-clicked';
+        Q103.id='Q203-clicked';
+    }
+    else{
+        U11.id='U02';
+        Q101P.id='Q201';
+        Q102P.id='Q202';
+        Q103P.id='Q203';
+    }
+}
+
+function clicked4(){
+    const U10 = document.getElementById('U03');
+    const U11 = document.getElementById('U03-clicked');
+    const Q101 = document.getElementById('Q301');
+    const Q101P = document.getElementById('Q301-clicked');
+    const Q102 = document.getElementById('Q302');
+    const Q102P = document.getElementById('Q302-clicked');
+    const Q103 = document.getElementById('Q303');
+    const Q103P = document.getElementById('Q303-clicked');
+    const Q104 = document.getElementById('Q304');
+    const Q104P = document.getElementById('Q304-clicked');
+
+    if(U10){
+        U10.id='U03-clicked';
+        Q101.id='Q301-clicked';
+        Q102.id='Q302-clicked';
+        Q103.id='Q303-clicked';
+        Q104.id='Q304-clicked';
+    }
+    else{
+        U11.id='U03';
+        Q101P.id='Q301';
+        Q102P.id='Q302';
+        Q103P.id='Q303';
+        Q104P.id='Q304';
+    }
+}
+
+function clicked5(){
+    const U10 = document.getElementById('U04');
+    const U11 = document.getElementById('U04-clicked');
+    const Q101 = document.getElementById('Q401');
+    const Q101P = document.getElementById('Q401-clicked');
+    const Q102 = document.getElementById('Q402');
+    const Q102P = document.getElementById('Q402-clicked');
+    const Q103 = document.getElementById('Q403');
+    const Q103P = document.getElementById('Q403-clicked');
+    const Q104 = document.getElementById('Q404');
+    const Q104P = document.getElementById('Q404-clicked');
+
+    if(U10){
+        U10.id='U04-clicked';
+        Q101.id='Q401-clicked';
+        Q102.id='Q402-clicked';
+        Q103.id='Q403-clicked';
+        Q104.id='Q404-clicked';
+    }
+    else{
+        U11.id='U04';
+        Q101P.id='Q401';
+        Q102P.id='Q402';
+        Q103P.id='Q403';
+        Q104P.id='Q404';
+    }
+}
+
+function clicked6(){
+    const U10 = document.getElementById('U05');
+    const U11 = document.getElementById('U05-clicked');
+    const Q101 = document.getElementById('Q501');
+    const Q101P = document.getElementById('Q501-clicked');
+    const Q102 = document.getElementById('Q502');
+    const Q102P = document.getElementById('Q502-clicked');
+    const Q103 = document.getElementById('Q503');
+    const Q103P = document.getElementById('Q503-clicked');
+    const Q104 = document.getElementById('Q504');
+    const Q104P = document.getElementById('Q504-clicked');
+
+    if(U10){
+        U10.id='U05-clicked';
+        Q101.id='Q501-clicked';
+        Q102.id='Q502-clicked';
+        Q103.id='Q503-clicked';
+        Q104.id='Q504-clicked';
+    }
+    else{
+        U11.id='U05';
+        Q101P.id='Q501';
+        Q102P.id='Q502';
+        Q103P.id='Q503';
+        Q104P.id='Q504';
+    }
+}
+
+function clicked7(){
+    const U10 = document.getElementById('U06');
+    const U11 = document.getElementById('U06-clicked');
+    const Q101 = document.getElementById('Q601');
+    const Q101P = document.getElementById('Q601-clicked');
+    const Q102 = document.getElementById('Q602');
+    const Q102P = document.getElementById('Q602-clicked');
+    const Q103 = document.getElementById('Q603');
+    const Q103P = document.getElementById('Q603-clicked');
+
+    if(U10){
+        U10.id='U06-clicked';
+        Q101.id='Q601-clicked';
+        Q102.id='Q602-clicked';
+        Q103.id='Q603-clicked';
+    }
+    else{
+        U11.id='U06';
+        Q101P.id='Q601';
+        Q102P.id='Q602';
+        Q103P.id='Q603';
+    }
+}
+
+function clicked8(){
+    const U10 = document.getElementById('U07');
+    const U11 = document.getElementById('U07-clicked');
+    const Q101 = document.getElementById('Q701');
+    const Q101P = document.getElementById('Q701-clicked');
+    const Q102 = document.getElementById('Q702');
+    const Q102P = document.getElementById('Q702-clicked');
+    const Q103 = document.getElementById('Q703');
+    const Q103P = document.getElementById('Q703-clicked');
+
+    if(U10){
+        U10.id='U07-clicked';
+        Q101.id='Q701-clicked';
+        Q102.id='Q702-clicked';
+        Q103.id='Q703-clicked';
+    }
+    else{
+        U11.id='U07';
+        Q101P.id='Q701';
+        Q102P.id='Q702';
+        Q103P.id='Q703';
+    }
+}
+
+function clicked9(){
+    const U10 = document.getElementById('U08');
+    const U11 = document.getElementById('U08-clicked');
+    const Q101 = document.getElementById('Q801');
+    const Q101P = document.getElementById('Q801-clicked');
+    const Q102 = document.getElementById('Q802');
+    const Q102P = document.getElementById('Q802-clicked');
+    const Q103 = document.getElementById('Q803');
+    const Q103P = document.getElementById('Q803-clicked');
+    const Q104 = document.getElementById('Q804');
+    const Q104P = document.getElementById('Q804-clicked');
+
+    if(U10){
+        U10.id='U08-clicked';
+        Q101.id='Q801-clicked';
+        Q102.id='Q802-clicked';
+        Q103.id='Q803-clicked';
+        Q104.id='Q804-clicked';
+    }
+    else{
+        U11.id='U08';
+        Q101P.id='Q801';
+        Q102P.id='Q802';
+        Q103P.id='Q803';
+        Q104P.id='Q404';
+    }
+}
+
+function clicked10(){
+    const U10 = document.getElementById('U09');
+    const U11 = document.getElementById('U09-clicked');
+    const Q101 = document.getElementById('Q901');
+    const Q101P = document.getElementById('Q901-clicked');
+    const Q102 = document.getElementById('Q902');
+    const Q102P = document.getElementById('Q902-clicked');
+    const Q103 = document.getElementById('Q903');
+    const Q103P = document.getElementById('Q903-clicked');
+    const Q104 = document.getElementById('Q904');
+    const Q104P = document.getElementById('Q904-clicked');
+
+    if(U10){
+        U10.id='U09-clicked';
+        Q101.id='Q901-clicked';
+        Q102.id='Q902-clicked';
+        Q103.id='Q903-clicked';
+        Q104.id='Q904-clicked';
+    }
+    else{
+        U11.id='U09';
+        Q101P.id='Q901';
+        Q102P.id='Q902';
+        Q103P.id='Q903';
+        Q104P.id='Q904';
+    }
+}
+
+function clicked11(){
+    const U10 = document.getElementById('U10');
+    const U11 = document.getElementById('U10-clicked');
+    const Q101 = document.getElementById('Q1001');
+    const Q101P = document.getElementById('Q1001-clicked');
+    const Q102 = document.getElementById('Q1002');
+    const Q102P = document.getElementById('Q1002-clicked');
+    const Q103 = document.getElementById('Q1003');
+    const Q103P = document.getElementById('Q1003-clicked');
+    const Q104 = document.getElementById('Q1004');
+    const Q104P = document.getElementById('Q1004-clicked');
+
+    if(U10){
+        U10.id='U10-clicked';
+        Q101.id='Q1001-clicked';
+        Q102.id='Q1002-clicked';
+        Q103.id='Q1003-clicked';
+        Q104.id='Q1004-clicked';
+    }
+    else{
+        U11.id='U10';
+        Q101P.id='Q1001';
+        Q102P.id='Q1002';
+        Q103P.id='Q1003';
+        Q104P.id='Q1004';
+    }
+}
+
+function clicked12(){
+    const U10 = document.getElementById('U11');
+    const U11 = document.getElementById('U11-clicked');
+    const Q101 = document.getElementById('Q1101');
+    const Q101P = document.getElementById('Q1101-clicked');
+    const Q102 = document.getElementById('Q1102');
+    const Q102P = document.getElementById('Q1102-clicked');
+    const Q103 = document.getElementById('Q1103');
+    const Q103P = document.getElementById('Q1103-clicked');
+    const Q104 = document.getElementById('Q1104');
+    const Q104P = document.getElementById('Q1104-clicked');
+
+    if(U10){
+        U10.id='U11-clicked';
+        Q101.id='Q1101-clicked';
+        Q102.id='Q1102-clicked';
+        Q103.id='Q1103-clicked';
+        Q104.id='Q1104-clicked';
+    }
+    else{
+        U11.id='U11';
+        Q101P.id='Q1101';
+        Q102P.id='Q1102';
+        Q103P.id='Q1103';
+        Q104P.id='Q1104';
+    }
+}
+
+function clicked13(){
+    const U10 = document.getElementById('U12');
+    const U11 = document.getElementById('U12-clicked');
+    const Q101 = document.getElementById('Q1201');
+    const Q101P = document.getElementById('Q1201-clicked');
+    const Q102 = document.getElementById('Q1202');
+    const Q102P = document.getElementById('Q1202-clicked');
+    const Q103 = document.getElementById('Q1203');
+    const Q103P = document.getElementById('Q1203-clicked');
+    const Q104 = document.getElementById('Q1204');
+    const Q104P = document.getElementById('Q1204-clicked');
+
+    if(U10){
+        U10.id='U12-clicked';
+        Q101.id='Q1201-clicked';
+        Q102.id='Q1202-clicked';
+        Q103.id='Q1203-clicked';
+        Q104.id='Q1204-clicked';
+    }
+    else{
+        U11.id='U12';
+        Q101P.id='Q1201';
+        Q102P.id='Q1202';
+        Q103P.id='Q1203';
+        Q104P.id='Q1204';
+    }
+}
